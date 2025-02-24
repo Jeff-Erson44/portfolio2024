03ee5fb04310d023bb79589d76861a3fcec0b2b2
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2025-02-24 16:57:51 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Dossier de sortie pour S3
+    emptyOutDir: true,
+  },
 })
 
